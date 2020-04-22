e8eafbee76a30c6942cf811d40d3b92376e10028
Arati <pradhan.arati14@gmail.com>
2020-04-22 14:05:04 +0530

    changed from static page to dynamic page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,57 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
 
-
  class App extends Component {
    constructor(props) {
      super(props)
    
      this.state = {
-      value: '',
-      data: '',
-      username:'',
-      msg:'',
-      src:'',
-      email:''
-      };
-      this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+        value: '',
+        data:'',
+        username:'',
+        src:'',
+        email:'',
+        fullname:''
+     }
+     this.handleChange = this.handleChange.bind(this);
+     this.handleSubmit = this.handleSubmit.bind(this);
    }
-   
-  handleChange(event) {
-    this.setState({value: event.target.value});
- 
-  }
-
- handleSubmit(event) 
-  {
-    const no=String(this.state.data.id)
-     if(this.state.value === no){
-      this.setState(
-        {username:this.state.data.login,
-        src:this.state.data.avatar_url,
-        email:this.state.data.email});
-    }
-    else{
-      this.setState({msg:'Incorrect Id'});
-    }
-    event.preventDefault();
-  }
- 
   
-  async  componentDidMount(){
-    const url = "https://api.github.com/users/example";
-    const response = await fetch(url);
-    const data = await response.json();
-    this.setState({data:data});
-    console.log(this.state.data.id);
-    
-    
-  }
-   
+   handleChange(event)
+    {
+      this.setState({value: event.target.value});
+    }
+    async handleSubmit(event) 
+      {
+        event.preventDefault();
+        const axios = require('axios');
+        const response = await axios.get(`https://api.github.com/users/${this.state.value}`);
+        this.setState({data:response,});
+        this.setState({username:this.state.data.data.login,
+        src:this.state.data.data.avatar_url,
+        email:this.state.data.data.email,
+        fullname:this.state.data.data.name});
+     }
+          
   render() {
     return (
-      <div >
+      <div>
         <h1 className="Heading">Github User Details</h1>
         <form onSubmit={this.handleSubmit}>
           <label className="text-area">enter the user id
@@ -60,13 +44,12 @@ import './App.css';
           <input className="submit-button" type="submit" value="fetch details" />
         </form>
         <div className="display-box">
-            <h2>{this.state.msg}</h2>
+            <img className="photo" src={this.state.src} alt=''></img>
             <h2>{this.state.username}</h2>
-            <img src={this.state.src}></img>
-            <h2>{this.state.username}</h2>
+            <h2>{this.state.fullname}</h2>
             <h2>{this.state.email}</h2>
-        </div>
-   
+         </div>
+       
       </div>
     )
   }
