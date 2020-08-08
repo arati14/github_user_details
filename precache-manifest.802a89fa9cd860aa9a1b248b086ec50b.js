236d1ea2cb588c56e53177b9852bc9a086fda299
Arati <pradhan.arati14@gmail.com>
2020-08-08 20:32:58 +0530

    Updates

---
+++ precache-manifest.802a89fa9cd860aa9a1b248b086ec50b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7233a5c6c34aae2fe515a38194b3341b",
+    "url": "/github_user_details/index.html"
+  },
+  {
+    "revision": "890670f3b5961bce7928",
+    "url": "/github_user_details/static/css/main.2d8222f2.chunk.css"
+  },
+  {
+    "revision": "0f66ad7d4cf8632ccbd8",
+    "url": "/github_user_details/static/js/2.fbee392c.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/github_user_details/static/js/2.fbee392c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "890670f3b5961bce7928",
+    "url": "/github_user_details/static/js/main.c10581e5.chunk.js"
+  },
+  {
+    "revision": "a429a3ab753c8a2ca3f8",
+    "url": "/github_user_details/static/js/runtime-main.05d3eaca.js"
+  }
+]);
